3f5f18f888fb387c0ede2aec2406570b93e53d21
Duan <leminhduan0262@gmail.com>
2024-10-20 14:57:14 +0700

    push drawable file

--- CSE441_CUSTOMER/app/build.gradle.kts
+++ CSE441_CUSTOMER/app/build.gradle.kts
@@ -40,4 +40,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
+
+    implementation ("com.tbuonomo:dotsindicator:4.3")
 }
