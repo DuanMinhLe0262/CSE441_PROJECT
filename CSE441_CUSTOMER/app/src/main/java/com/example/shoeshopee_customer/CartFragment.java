454522f1758ab15144d145c8ef4c38159091552f
hhiennnvu123 <152370161+hhiennnvu123@users.noreply.github.com>
2024-10-31 01:09:31 +0700

    layout main, home fragment

---
+++ CSE441_CUSTOMER/app/src/main/java/com/example/shoeshopee_customer/CartFragment.java
@@ -0,0 +1,34 @@
+package com.example.shoeshopee_customer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class CartFragment extends Fragment {
+
+    private static final String ARG_USER_ID = "user_id";
+
+    public static NotificationFragment newInstance(String userId) {
+        NotificationFragment fragment = new NotificationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_USER_ID, userId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        if (getArguments() != null) {
+            String userId = getArguments().getString(ARG_USER_ID);
+            // Use the userId as needed
+        }
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
